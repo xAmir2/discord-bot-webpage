95242ce6d671268631b0f62549a393a535b48e96
Amir B. Kahna <amir.kahna2@gmail.com>
2026-09-21 10:32:31 +0200

    Initial commit

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "./Navbar";
+import { MainContent } from "./MainContent";
+import { Footer } from "./Footer";
+
+export function Homepage() {
+  return (
+    <>
+      <Navbar />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/MainContent.tsx
@@ -0,0 +1,3 @@
+export function MainContent() {
+  return <main></main>;
+}
